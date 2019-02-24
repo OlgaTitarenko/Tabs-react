0a1e36715d109b0c3c9c2c0263d8fda1d63c654b
титаренко <kizuneworld@gmail.com>
2019-02-24 03:05:28 +0200

    Updates

---
+++ precache-manifest.bb92d13c046e8915eef11da06fe112ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "08e37f32075bee95c42f",
+    "url": "/tabs-react/static/js/runtime~main.08e37f32.js"
+  },
+  {
+    "revision": "d49d5b849fabe7da08b9",
+    "url": "/tabs-react/static/js/main.d49d5b84.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/tabs-react/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "d49d5b849fabe7da08b9",
+    "url": "/tabs-react/static/css/main.ef20bab8.chunk.css"
+  },
+  {
+    "revision": "9c83a1b6cb944f6cd04d2f8a6a906230",
+    "url": "/tabs-react/index.html"
+  }
+];
